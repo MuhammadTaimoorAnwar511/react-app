ac83c7f9d9ac4d09abbbf2060d12c70c5deb503f
Muhammad Taimoor Anwar <129211565+MuhammadTaimoorAnwar511@users.noreply.github.com>
2025-05-02 15:57:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Second TRY
+          First TRY
         </p>
         <a
           className="App-link"
